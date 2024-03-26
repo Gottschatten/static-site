d7035c5a39a3ca78366e100ffe7615f6e5c49d09
Gottschatten <Marten.Drews@yahoo.de>
2024-03-26 12:35:08 +0100

    Add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,3 +15,19 @@ class HTMLNode:
       for prop in self.props:
          html += f' {prop}="{self.props[prop]}"'
       return html
+
+class LeafNode(HTMLNode):
+   def __init__(self, tag=None, value=None, children=None, props=None) -> None:
+      super().__init__(tag, value, children, props)
+
+   def to_html(self):
+      if self.value is None:
+         raise ValueError
+      if self.tag is None:
+         return self.value
+      if self.props is None:
+         html = f"<{self.tag}>" + self.value + f"</{self.tag}>"
+         return html
+      html = f'<{self.tag}{self.props_to_html()}>' + self.value + f'</{self.tag}>'
+      return html
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
    def test_toHtml_excep(self):
@@ -12,6 +12,18 @@ class TestHTMLNode(unittest.TestCase):
                       "target": "_blank"
       })
       self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
+class TestLeafNode(unittest.TestCase):
+   def test_toHtml(self):
+      node = LeafNode(tag="p", value="This is a paragraph of text!")
+      self.assertEqual(node.to_html(), "<p>This is a paragraph of text!</p>")
+      node2 = LeafNode(tag="a", value="Click me!", props={"href": "https://www.google.com"})
+      self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+
+   def test_noValuefail(self):
+      node = LeafNode()
+      self.assertRaises(ValueError, node.to_html)
 
 
 if __name__== "__main__":
